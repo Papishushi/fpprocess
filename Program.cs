bab2d8ab1722eaba46b316b3ac358c53396d6652
Daniel Molinero Lucas <danielmolinero111@gmail.com>
2022-08-13 02:21:21 +0200

    Tons of changes!!
    * Better looking colors!
    * A lot of bug fixing, yeah!
    * Added support for process names, instead of PIDs. (You can mix inputs)
    * Added support for parameters.
    * Added new functionality for changing background color to WriteLineColor method.
    * Color output mode switch with --no-color parameter
    * Now you can run the script at an interval with -i parameter

--- Program.cs
+++ Program.cs
@@ -1,76 +1,134 @@
 ﻿using System.Diagnostics;
 
 bool running = true;
+bool isColorOutput = true;
+bool isIntervalMode = false;
+Stopwatch stopWatch = new Stopwatch();
+int interval = 1500;
 
 void StopRunning(object? sender, ConsoleCancelEventArgs args) => running = false;
 
 Console.CancelKeyPress += StopRunning;
 
-foreach (var arg in args)
+if (args.Length > 0 && args.Contains("--no-color"))
 {
-    var nameMode = false;
-    var tab = string.Empty;
-    var tid = 0;
-    var id = 0;
-    Process p = Process.GetProcessById(0);
+    isColorOutput = false;
+    args = args.Except(new[] { "--no-color" }).ToArray();
+}
 
-    try
+if (args.Length > 0 && args.Contains("-i"))
+{
+    var temp = string.Empty;
+    for (int i = 0; i < args.Length; i++)
+        if (args[i] == "-i")
+        {
+            try
+            {
+                temp = args[i + 1];
+                interval = Convert.ToInt32(temp);
+                break;
+            }
+            catch
+            {
+                return;
+            }
+        }
+    isIntervalMode = true;
+    args = args.Except(new[] { "-i" , temp}).ToArray();
+}
+
+stopWatch.Start();
+do
+{
+    if (stopWatch.ElapsedMilliseconds < interval && isIntervalMode) continue;
+    stopWatch.Restart();
+    MainAlgorithm();
+} while (isIntervalMode);
+stopWatch.Stop();
+
+void MainAlgorithm()
+{
+    foreach (var arg in args)
     {
-        id = Convert.ToInt32(arg);
-    }
-    catch
-    {
+        var nameMode = false;
+        var tab = string.Empty;
+        var tid = 0;
+        var id = 0;
+        Process p = Process.GetProcessById(0);
+
         try
         {
-            p = Process.GetProcessesByName(arg).First();
+            id = Convert.ToInt32(arg);
         }
         catch
         {
-            WriteLineColor($"\nArgument not correctly formatted '{arg}'", ConsoleColor.DarkRed);
-            continue;
-        }
-
-        nameMode = true;
-    }
-
-    while (running)
-    {
-        if (tid != 0)
-            id = tid;
-        try
-        {
-            if (tid != 0 || !nameMode)
-                p = Process.GetProcessById(id);
-            var parent = GetParent(p);
-            if (parent == null) break;
-
-            WriteLineColor($"\n{tab}Get Info from process {p.Id} ({p.ProcessName})", ConsoleColor.Green);
-
-            tid = parent.Id;
-            if (tid == 0) break;
-
-            DisplayProcessInfo(tab, p, parent);
-
-            if (parent.HasExited)
+            try
             {
-                WriteLineColor($"{tab}Process {parent.Id} has exited... Aborting operations", ConsoleColor.DarkYellow);
+                p = Process.GetProcessesByName(arg).First();
+                nameMode = true;
+            }
+            catch
+            {
+                WriteLineColor($"\nArgument not correctly formatted or process not running '{arg}'", isColorOutput ? ConsoleColor.DarkRed : Console.ForegroundColor);
                 continue;
             }
         }
-        catch (Exception ex)
-        {
-            WriteLineColor($"{tab}{ex.Message} {ex.InnerException?.Message}", ConsoleColor.DarkRed);
-            if (ex.GetType() == typeof(ArgumentException)) break;
-            tab += "\t";
-            continue;
-        }
 
-        tab += "\t";
+        Process? parent = null;
+
+        while (running)
+        {
+            if (tid != 0)
+                id = tid;
+            try
+            {
+                if (tid != 0 || !nameMode) p = Process.GetProcessById(id);
+                if (p.HasExited) break;
+
+                WriteLineColor($"\n{tab}Get Info from process {p.Id} ({p.ProcessName})", isColorOutput ? ConsoleColor.Green : Console.ForegroundColor);
+
+                parent = GetParent(p);
+
+                if (parent != null)
+                {
+                    tid = parent.Id;
+                    if (tid == 0) break;
+                }
+
+                if (isColorOutput)
+                    DisplayProcessInfo(tab, p, parent);
+                else
+                    DisplayProcessInfoWhite(tab, p, parent);
+
+                if (parent == null) break;
+                else if (parent.HasExited)
+                {
+                    tab += "\t";
+                    WriteLineColor($"\n{tab}Process {parent.Id} has exited... Aborting operations", isColorOutput ? ConsoleColor.DarkYellow : Console.ForegroundColor);
+                    break;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                WriteLineColor($"\n{ex.Message} {ex.InnerException?.Message}", isColorOutput ? ConsoleColor.DarkRed : Console.ForegroundColor);
+                break;
+            }
+            catch (Exception ex)
+            {
+                WriteLineColor($"{ex.Message} {ex.InnerException?.Message}", isColorOutput ? ConsoleColor.DarkRed : Console.ForegroundColor);
+                if (parent == null) break;
+                tab += "\t";
+                continue;
+            }
+
+            tab += "\t";
+        }
     }
 }
 
-static void WriteLineColor(string content, ConsoleColor color)
+static void WriteLineColor(string content, ConsoleColor color, params ConsoleColor[] background)
 {
+    Console.BackgroundColor = background.FirstOrDefault();
     Console.ForegroundColor = color;
     Console.WriteLine(content);
     Console.ResetColor();
@@ -97,7 +155,6 @@ static string FindIndexedProcessName(int pId)
     {
         return string.Empty;
     }
-
 }
 
 static Process? FindPidFromIndexedProcessName(string indexedProcessName)
@@ -116,9 +173,18 @@ static Process? GetParent(Process process) => FindPidFromIndexedProcessName(Find
 
 static void DisplayProcessInfo(string tab, Process p, Process? parent)
 {
-    Console.WriteLine($"{tab}Parent PID: {parent?.Id}");
-    Console.WriteLine($"{tab}Name: {p.ProcessName}");
-    Console.WriteLine($"{tab}Working Set (64): {(float)(p.WorkingSet64 * 0.0000001)} mb");
-    Console.WriteLine($"{tab}Module Name: {p.MainModule?.ModuleName}");
-    Console.WriteLine($"{tab}Module Path: {p.MainModule?.FileName}");
+    Console.Write($"{tab}Parent PID         |  "); WriteLineColor($"{parent?.Id}", ConsoleColor.Gray, ConsoleColor.DarkBlue);
+    Console.Write($"{tab}Name               |  "); WriteLineColor($"{p.ProcessName}", ConsoleColor.Gray);
+    Console.Write($"{tab}Working Set (x64)  |  "); WriteLineColor($"{(float)(p.WorkingSet64 * 0.0000001)} mb", ConsoleColor.Gray);
+    Console.Write($"{tab}Module Name        |  "); WriteLineColor($"{p.MainModule?.ModuleName}", ConsoleColor.DarkMagenta);
+    Console.Write($"{tab}Module Path        |  "); WriteLineColor($"{p.MainModule?.FileName.Replace(p.MainModule?.ModuleName ?? string.Empty, string.Empty)}", ConsoleColor.DarkMagenta);
+}
+
+static void DisplayProcessInfoWhite(string tab, Process p, Process? parent)
+{
+    Console.WriteLine($"{tab}Parent PID         |  {parent?.Id}");
+    Console.WriteLine($"{tab}Name               |  {p.ProcessName}");
+    Console.WriteLine($"{tab}Working Set (x64)  |  {(float)(p.WorkingSet64 * 0.0000001)} mb");
+    Console.WriteLine($"{tab}Module Name        |  {p.MainModule?.ModuleName}");
+    Console.WriteLine($"{tab}Module Path        |  {p.MainModule?.FileName.Replace(p.MainModule?.ModuleName ?? string.Empty, string.Empty)}"); 
 }
